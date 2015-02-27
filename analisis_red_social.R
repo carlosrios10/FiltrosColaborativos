ee54d4e1c146cab16c5889470fd137b40f9ed523
carlosrios10 <cm.rios.10@gmail.com>
2015-02-27 18:23:27 -0300

    obtengo la red social reducida

--- analisis_red_social.R
+++ analisis_red_social.R
@@ -25,10 +25,8 @@ socialgraphData$second_user_id<-as.character(socialgraphData$second_user_id)
 
 sum(vertices%notin%socialgraphData$first_user_id)
 sum(socialgraphData$first_user_id==vertices)
-which (!is.element(vertices, socialgraphData$first_user_id))
+vertices[which (!is.element(vertices, socialgraphData$first_user_id))]
 vertex<-vertices[which (is.element(vertices, socialgraphData$first_user_id))]
-
-
 
 grafo<-graph.data.frame(socialgraphData, directed=T, vertices=NULL)
 subv <- c("1228393")
@@ -39,16 +37,20 @@ grafoReducido<-induced.subgraph(grafo, vids=vertex)
 plot(grafoReducido, layout=layout.fruchterman.reingold, vertex.size=4,
      vertex.label.dist=0.5, vertex.color="red", edge.arrow.size=0.5)
 grafoReducidoDF<-get.data.frame(grafoReducido, what=c( "edges"))
-
 head(grafoReducidoDF,n=20)
 tail(grafoReducidoDF)
 str(grafoReducidoDF)
 
-grafoReducidoDF10<-grafoReducidoDF[grafoReducidoDF$from==10,]
+write.table(grafoReducidoDF,file="C:/Users/Usuarioç/Desktop/carlos/Tesis/datasets/foursquare/datasets_csv/redSocialReducida.csv", 
+          row.names = F, col.names= F,sep=",",quote = F)
+
 
 #medidas de centralidad
 is.directed(grafo)
-max(degree(grafo))
+degree(grafoReducido,v=c("1"),mode="in")
+max(degree(grafoReducido))
+min(degree(grafoReducido))
+
 plot(degree.distribution(grafo))
 summary(grafo)
 adj<-get.adjacency(grafo)
